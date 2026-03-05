de530ec943b224d05f9ebf02f350b39cb1276367
muxammadalicoder <muhammadali20011503@gmail.com>
2026-03-05 12:03:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import google.generativeai as genai
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -17,21 +17,32 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 @bot.message_handler(func=lambda message: True)
 def chat(message):
     try:
+        bot.send_chat_action(message.chat.id, "typing")
+
         response = model.generate_content(message.text)
-        bot.reply_to(message, response.text)
+
+        if response.text:
+            bot.reply_to(message, response.text)
+        else:
+            bot.reply_to(message, "Javob topilmadi")
+
     except Exception as e:
+        print(e)
         bot.reply_to(message, "Xatolik yuz berdi")
 
 
-@app.route("/", methods=["GET", "POST"])
-def webhook():
-    if request.method == "POST":
-        json_str = request.get_data().decode("UTF-8")
-        update = telebot.types.Update.de_json(json_str)
-        bot.process_new_updates([update])
-        return "ok", 200
+@app.route("/", methods=["GET"])
+def home():
     return "Bot ishlayapti"
 
 
+@app.route("/", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
