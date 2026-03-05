469765920857b602e8d5b9ea7d3d8210a8bfc99c
muxammadalicoder <muhammadali20011503@gmail.com>
2026-03-05 10:57:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+from flask import Flask, request
 import telebot
 import google.generativeai as genai
-from flask import Flask, request
+
+app = Flask(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -9,33 +11,30 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-pro")
-
-app = Flask(__name__)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/")
 def home():
     return "Bot ishlayapti"
 
-@app.route("/webhook", methods=["POST"])
+@app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def webhook():
     json_str = request.get_data().decode("UTF-8")
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return "OK", 200
+    return "ok"
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message,"Salom! Men Gemini AI botman. Savol bering.")
 
 @bot.message_handler(func=lambda message: True)
-def message(message):
-    user_text = message.text
-
+def ai(message):
     try:
-        response = model.generate_content(user_text)
-        bot.reply_to(message, response.text)
+        response = model.generate_content(message.text)
+        bot.reply_to(message,response.text)
     except:
-        bot.reply_to(message, "Xatolik yuz berdi")
+        bot.reply_to(message,"Xatolik yuz berdi.")
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url="https://telegram-taqdimot-bot.onrender.com/webhook")
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0",port=10000)
